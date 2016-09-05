48c5bed733064fd53358734e4fe11b9a57522ae4
91juhwang <91juhwang@gmail.com>
2016-09-05 16:14:47 -0400

    basic angular functionality

--- public/app.js
+++ public/app.js
@@ -3,6 +3,27 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
 	'$scope',
 	function($scope){
-	  $scope.test = 'Hello world!';
+	  $scope.posts = [
+	  	{title: "post 1", upvotes: 5},
+	  	{title: "post 2", upvotes: 14},
+	  	{title: "post 3", upvotes: 0},
+	  	{title: "post 4", upvotes: 4},
+	  	{title: "post 5", upvotes: 12}
+	  ];
+
+	  $scope.addPost = function() {
+	  	if ($scope.title == "" || $scope.title == null) { return alert("Please enter the title")}
+	  	$scope.posts.push({
+	  		title: $scope.title, 
+	  		link: $scope.link,
+	  		upvotes: 0
+	  	});
+	  	$scope.title = "";
+	  	$scope.link = "";
+	  }
+
+	  $scope.incrementUpvotes = function(post) {
+	  	post.upvotes += 1
+	  }
 	}
 ]);
